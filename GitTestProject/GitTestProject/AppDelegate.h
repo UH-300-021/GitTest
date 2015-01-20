4558dd92880f049bc58bc7de1f80b8bb215da305
Matthew York <myork@cs.ua.edu>
2015-01-20 17:05:09 -0600

    - First Project Commit

---
+++ GitTestProject/GitTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTestProject
+//
+//  Created by UH 300 -21 on 1/20/15.
+//  Copyright (c) 2015 UH 300 -21. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTestProject/GitTestProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTestProject
+//
+//  Created by UH 300 -21 on 1/20/15.
+//  Copyright (c) 2015 UH 300 -21. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
